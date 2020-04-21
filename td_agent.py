22313aa8dca28b6c3dcf7daee7356cbcd2dce444
Tristan Shah <123.tristan.shah@gmail.com>
2020-04-21 13:17:10 -0400

    There is a mistake in td_agent.py

--- td_agent.py
+++ td_agent.py
@@ -1,18 +1,21 @@
 import numpy as np
 from utils import *
 
-def train_td(gamma=1, alpha=0.1, episodes=20000):
+def train_td(gamma=1, alpha=0.1, episodes=200000):
 	q_table = np.ones((2,22,11,2))
 	pi = np.ones((2,22,11))
 	for episode in range(episodes):
 		states, actions, rewards = generate_episode(pi)
 		state = states[0]
 		for i in range(1,len(states)):
+
 			current_q = q_table[state[0]][state[1]][state[2]][int(actions[i])]
+			## THERE IS FLAWED LOGIC HERE
+			## This is saying the next q_value is equal to the next state, and previous action
 			next_q = q_table[states[i][0]][states[i][1]][states[i][2]][int(actions[i])]
 
 			## Temporal difference update
-			q_table[state[0]][state[1]][state[2]][int(actions[i])] = current_q + alpha*(rewards[i] + gamma*next_q - current_q)
+			q_table[state[0]][state[1]][state[2]][int(actions[i])] = current_q + alpha*(rewards[i-1] + gamma*next_q - current_q)
 
 			## Greedify
 			A = np.argmax(q_table[state[0]][state[1]][state[2]])
